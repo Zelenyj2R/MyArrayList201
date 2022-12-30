1ab0c84d4c036c5c7e01957efc860458ffefa179
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2022-12-30 10:00:21 +0100

    Update MyArrayList.java
    удалены пробелы и добавленны {}

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,38 +1,32 @@
-public class MyArrayList <L> {
-
-    private  Object[] deliveryCities = new Object[10];
+public class MyArrayList<L> {
+    private Object[] myArrayList = new Object[10];
     private int position = 0;
-
-        public void add(L value){
-            deliveryCities[position] = value;
-            System.out.println(position+"  "+deliveryCities[position]);position++;
-
-        }
-    public L get(int position) {
-        return (L) deliveryCities[position];
-
+    public void add(L value) {
+        myArrayList[position] = value;
+        System.out.println(position + "  " + myArrayList[position]);
+        position++;
     }
-
-
+    public L get(int position) {
+        return (L) myArrayList[position];
+    }
     public int size() {
         return position;
     }
-
     public void clear() {
-        for (int i= 0; i<=position; i++)
-            deliveryCities[position] = null;
+        for (int i = 0; i <= position; i++) {
+            myArrayList[position] = null;
+        }
         position = 0;
     }
-
     public boolean remove(int index) {
-        if(index<position) {
-            for (int i = index; i < position; i++)
-                deliveryCities[i] = deliveryCities[i + 1];
-            deliveryCities[position] = null;
+        if (index < position) {
+            for (int i = index; i < position; i++) {
+                myArrayList[i] = myArrayList[i + 1];
+            }
+            myArrayList[position] = null;
             position--;
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
